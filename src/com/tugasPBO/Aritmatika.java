1aae9f57acbec26567bba27132866e7bdb4cb811
unknown <argya.programmer@gmail.com>
2020-10-17 14:40:33 +0700

    Initial Commit

---
+++ src/com/tugasPBO/Aritmatika.java
@@ -0,0 +1,29 @@
+// latihan 12 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan aritmatika tambah, kurang, kali, bagi,
+//	            modulus, increment dan decrement
+package com.tugasPBO;
+
+public class Aritmatika {
+    public static void main(String[] args){
+
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = "+ (b--));
+        System.out.println("d++ = "+ (d++));
+        System.out.println("++d = "+(++d));
+    }
+}
+
